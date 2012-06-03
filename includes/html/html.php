0b85a4a612f9f1817f36fa9007763c67798c8fd5
Adam Shannon <adam@ashannon.us>
2012-06-03 15:46:32 -0500

    Move the one image to the cdn.

--- includes/html/html.php
+++ includes/html/html.php
@@ -10,7 +10,7 @@
 	<title>Adam Shannon - Open Web Supporter</title>
 
 	<link rel="stylesheet" type="text/css" href="includes/css/style.css" />
-	<link rel="shortcut icon" href="images/favcon.jpg" />
+	<link rel="shortcut icon" href="http://cdn.ashannon.us/images/favcon.jpg" />
 
 	<meta charset="UTF-8" />
 	<meta name='verify-v1' content='RJ65OaEN3MUHe6d/2QIR+pvTXj6YICqyxKx31tfc46s=' />
