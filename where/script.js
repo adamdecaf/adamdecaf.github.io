efe9d7ff5673e989aeecfc602398edebefd33cf4
Adam Shannon <adamkshannon@gmail.com>
2017-09-24 22:12:50 -0500

    rm todo

--- where/script.js
+++ where/script.js
@@ -13,8 +13,6 @@ function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 };
 
-// TODO(adam): planet fitnesses
-
 function addGMap(coords) {
   var ifr = document.createElement("iframe");
   ifr.src = "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d190253.31038971894!2d" + coords.longitude + "!3d" + coords.latitude + "!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1slocal+attractions!5e0!3m2!1sen!2sus!4v1506306393285"
