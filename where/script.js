cca0a4abc5796ee897d915d8a564cd5d53d376ee
Adam Shannon <adamkshannon@gmail.com>
2017-09-28 14:38:01 -0500

    where: add nfs maps

--- where/script.js
+++ where/script.js
@@ -36,3 +36,41 @@ navigator.geolocation.getCurrentPosition(success, error, {
   timeout: 10000, // 10s, in ms
   maximumAge: 0
 });
+
+// Add National Forest Service maps
+
+// Format: 201727
+function getNFSShortDateYesterday() {
+  var dt = new Date();
+  var year = 1900 + dt.getYear()
+  var day = dt.getDate() - 1
+  return year.toString() + day.toString();
+}
+console.log(getNFSShortDateYesterday());
+
+// From https://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd
+function getYYYYMMDD() {
+  var d = new Date();
+  var month = '' + (d.getMonth() + 1);
+  var day = '' + d.getDate();
+  var year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+console.log(getYYYYMMDD());
+
+// Set NFS image links
+function nfsSet(id, url) {
+  var elm = document.querySelector("#" + id);
+  elm.src = url;
+}
+nfsSet("nfs-fd", "https://fsapps.nwcg.gov/afm/data/last12/last12_" + getNFSShortDateYesterday() + "1_1200_fd_pts.png");
+nfsSet("nfs-rf", "https://fsapps.nwcg.gov/afm/data/last12/last12_" + getNFSShortDateYesterday() + "1_1200_fw_pts.png");
+nfsSet("nfs-ld", "https://fsapps.nwcg.gov/afm/data/last12/last12_" + getNFSShortDateYesterday() + "1_1200_ls_pts.png");
+
+// Large Fire Incidents
+nfsSet("nfs-lf-fire-img", "https://fsapps.nwcg.gov/data/lg_fire/lg_fire_nifc_" + getYYYYMMDD() + ".png");
+document.querySelector("#nfs-lg-fire-a").href = "https://fsapps.nwcg.gov/data/lg_fire/lg_fire_nifc_" + getYYYYMMDD() + "_browse.png";
